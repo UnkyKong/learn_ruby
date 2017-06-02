bef8bb34c9dd7a25d83e6fe2609418ec109025f0
Matthew Uncapher <unky.codes@gmail.com>
2017-06-02 08:47:26 -0400

    Finished first three folders

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(ftemp)
+    (ftemp.to_f - 32.0) * (5.0/9.0)
+end
+
+def ctof(ctemp)
+    (ctemp / (5.0/9.0)) + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b 
+end
+
+def sum (array)
+    sum = 0
+    array.each do |x|
+        sum += x
+    end
+    sum
+end
+
